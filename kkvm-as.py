f44255b1631c54e0669394570b6c663e1866f800
Valeriy K <valeriykireev@gmail.com>
2016-02-20 00:30:42 +0300

    Added working with argv[]

--- kkvm-as.py
+++ kkvm-as.py
@@ -1,7 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
 import lexycs, preprocessor, translate
 
 def main():
-    fp = open('./src.s', 'r')
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print(
+'''Usage:
+{} <source> [binary]
+
+    Whan binary file is missing, output will named "a.kk"'''.format(sys.argv[0]))
+        sys.exit(0)
+    
+    try:
+        fp = open(sys.argv[1], 'r')
+    except IOError:
+        print("{}: can't open file {}".format(sys.argv[0], sys.argv[1]))
+        sys.exit(1)
+        
     source = []
     for line in fp:
         source.append(line.split())
@@ -14,7 +31,17 @@ def main():
     if program == None:
         return 2
     code = translate.translate(data, program)
-    fp = open('./a.out', 'wb')
+    
+    if (len(sys.argv) == 3):
+        fout = sys.argv[2]
+    else:
+        fout = 'a.kk'
+    try:
+        fp = open(fout, 'wb')
+    except IOError:
+        print("{}: can't open file {}".format(sys.argv[0], fout))
+        sys.exit(1)
+    
     fp.write(code)
     fp.close()
     
